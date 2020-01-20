7ba4da804b3166fc3f706a65cc14e2018585eed2
Kiboot <kiboot@users.noreply.github.com>
2020-01-20 21:42:14 +0800

    Initial commit

---
+++ app/src/main/java/kibs/mcm/edu/ph/externalio/MainActivity.java
@@ -0,0 +1,14 @@
+package kibs.mcm.edu.ph.externalio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
